dae273de4754899f451748f67d028ca9da10017f
Paul Darragh <pmd6624@rit.edu>
2018-03-19 17:07:20 -0400

    added sass into cra

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 //Components
 import App from './components/App';
+import './assets/stylesheets/css/index.css';
 
 const store = configureStore();
 
